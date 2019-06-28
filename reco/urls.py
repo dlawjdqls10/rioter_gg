9e97fa7ffd890e8b2bcd4f4c21b620f4e706cd65
dlawjdqls10 <dlawjdqls16@gmail.com>
2019-06-28 20:20:54 +0900

    ..

--- reco/urls.py
+++ reco/urls.py
@@ -3,7 +3,7 @@ from reco import views
 
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('recommend', views.recommend, name='recommend'),
     path('riot.txt', views.riot, name='riot')
 ]
